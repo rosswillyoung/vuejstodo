fa6aab3afb981a80850a307728aac5a8052dc062
Ross Young <rosswillyoung@gmail.com>
2020-09-28 11:49:24 -0400

    Very basic todo app, liking it so far

---
+++ README.md
@@ -0,0 +1,2 @@
+### Testing out VueJS
+I am experimenting with VueJS to see how I like it and maybe convert my React project to Vue. 
--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const app = new Vue({
     el: '#app',
     data: {
-        title: 'Hello Ross'
+        title: 'Hello Ross',
+        newTodo: '',
+        todos: []
     },
     methods: {
         addTodo() {
-            console.log('Form submitted');
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = ''
+        },
+        removeTodo(todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+        allDone() {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            })
         }
     }
 })
